64d87b6aaf6427b9a6e8804dcba90f8dff902e29
Samuel Bächler <baechler@boeser.ch>
2020-11-22 13:57:53 +0100

    minor

--- tests/test_popstuff.py
+++ tests/test_popstuff.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Testing package ppl.
+No comment.
 """
 
 import pytest
@@ -12,6 +12,6 @@ from ppl.popstuff import value_plus_3
 @pytest.mark.parametrize("test_input,expected", [(3, 6), (33, 36), (-1, 2)])
 def test_value_plus3(test_input, expected):
     """
-    This text is for pylint.
+    No comment.
     """
     assert value_plus_3(test_input) == expected
