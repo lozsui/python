458a0acf2f18ab083e52bc635a26d8eef8ecda75
Samuel Bächler <baechler@boeser.ch>
2020-11-23 17:11:41 +0100

    'merge' two excel files using pandas dataframes

--- ppl/popstuff.py
+++ ppl/popstuff.py
@@ -4,6 +4,10 @@
 Idiosyncratic python things.
 """
 
+import os
+
+import pandas as pd
+
 
 def value_plus_3(arg):
     """
@@ -11,3 +15,59 @@ def value_plus_3(arg):
     :return: Number
     """
     return arg + 3
+
+
+def using_os():
+    """
+    :return:
+    """
+    os.chdir('../files')
+    file_list = os.listdir()
+    if 'aaa' in file_list:
+        return 'aaa'
+    return None
+
+
+def get_coordinates(author_to_find):
+    """
+    Pass the name of an author. Get row number and column number of
+    'cell' where author is found. It is possibly not the smartest wqy
+    to do that.
+    """
+    os.chdir('../files')
+    my_df = pd.read_excel('xlsx_B.xlsx')
+    column_number = 0
+    name_number_map = {}
+    for col in my_df.columns:
+        name_number_map[col] = column_number
+        column_number += 1
+    row_column_tuple = None
+    for row in my_df.itertuples():
+        author = row.Author
+        if author == author_to_find:
+            row_column_tuple = row.Index, name_number_map['Author']
+    return row_column_tuple
+
+
+def merge_xlsx():
+    """
+    Fill column Author in xlsx_A.xlsx with values found in xlsx_B.xlsx. Create
+    xlsx_C.xlsx with merged Author information.
+    """
+    os.chdir('../files')
+    my_df_1 = pd.read_excel('xlsx_A.xlsx')
+    my_df_2 = pd.read_excel('xlsx_B.xlsx')
+    col_name_to_int_map = {
+        'Author': 2,
+        'Function': 0,
+    }
+    for row in my_df_1.itertuples():
+        author = str(row[col_name_to_int_map['Author'] + 1])
+        function = row[col_name_to_int_map['Function'] + 1]
+        for other_row in my_df_2.itertuples():
+            other_author = other_row[col_name_to_int_map['Author'] + 1]
+            other_function = other_row[col_name_to_int_map['Function'] + 1]
+            if function == other_function and author == 'nan':
+                col_number = col_name_to_int_map['Author']
+                my_df_1.iat[row.Index, col_number] = other_author
+    my_df_1.to_excel('xlsx_C.xlsx')
--- tests/test_popstuff.py
+++ tests/test_popstuff.py
@@ -4,9 +4,11 @@
 No comment.
 """
 
+import pandas as pd
+
 import pytest
 
-from ppl.popstuff import value_plus_3
+import ppl.popstuff as popstuff
 
 
 @pytest.mark.parametrize("test_input,expected", [(3, 6), (33, 36), (-1, 2)])
@@ -14,4 +16,29 @@ def test_value_plus3(test_input, expected):
     """
     No comment.
     """
-    assert value_plus_3(test_input) == expected
+    assert popstuff.value_plus_3(test_input) == expected
+
+
+@pytest.mark.parametrize("expected", ['aaa'])
+def test_using_os(expected):
+    """
+    No comment.
+    """
+    assert popstuff.using_os() == expected
+
+
+def test_get_coordinates():
+    """
+    No comment.
+    """
+    assert popstuff.get_coordinates('Peter Gugelhopf') == (1, 2)
+
+
+def test_merge_xlsx():
+    """
+    No comment.
+    """
+    popstuff.merge_xlsx()
+    my_df = pd.read_excel('xlsx_C.xlsx')
+    author = my_df.iloc[1, 3]
+    assert author == 'Peter Gugelhopf'
