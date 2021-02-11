1d52ef85ae44157645fd999fc961accc50672af5
Samuel Bächler <baechler@boeser.ch>
2021-02-11 18:38:13 +0100

    mute pylint warnings

--- tests/test_fp.py
+++ tests/test_fp.py
@@ -8,32 +8,47 @@ import ppl.fp as fp
 
 
 def test_to_string():
-    v = fp.Vector(3, 8)
-    str_v = str(v)
+    """
+    How __repr__ is used behind the scene.
+    """
+    my_vector = fp.Vector(3, 8)
+    str_v = str(my_vector)
     assert str_v == 'Vector(3, 8)'
 
 
 def test_vector_addition():
-    v1 = fp.Vector(1, 2)
-    v2 = fp.Vector(3, 4)
-    v3 = v1 + v2
-    assert v3.x == 4
-    assert v3.y == 6
+    """
+    Operator overloading example.
+    """
+    v_1 = fp.Vector(1, 2)
+    v_2 = fp.Vector(3, 4)
+    v_3 = v_1 + v_2
+    assert v_3.x == 4
+    assert v_3.y == 6
 
 
 def test_multiply_vector_with_scalar():
-    v1 = fp.Vector(1, 2)
-    v2 = v1 * 5
-    assert v2.x == 5
-    assert v2.y == 10
+    """
+    Another operator overloading example.
+    """
+    v_1 = fp.Vector(1, 2)
+    v_2 = v_1 * 5
+    assert v_2.x == 5
+    assert v_2.y == 10
 
 
 def test_absolute_vector_value():
-    v = fp.Vector(4, 3)
-    assert 5 == abs(v)
+    """
+    How __abs__ is used behind the scene.
+    """
+    v_1 = fp.Vector(4, 3)
+    assert abs(v_1) == 5
 
 
 def test_my_listcomp_one():
+    """
+    List comprehension ~ listcomp
+    """
     one_two_three = [1, 2, 3, ]
     squared = fp.square_numbers(one_two_three)
     assert squared[0] == 1
@@ -42,6 +57,9 @@ def test_my_listcomp_one():
 
 
 def test_two_for_loops_listcomp_one():
+    """
+    Kind of nested listcomp.
+    """
     dishes = fp.create_dishes()
     assert len(dishes) == 4
     assert dishes[0] == ('rigatoni', 'al pesto')
@@ -51,6 +69,9 @@ def test_two_for_loops_listcomp_one():
 
 
 def test_two_for_loops_listcomp_two():
+    """
+    Shows outcome when nesting is in reversed order.
+    """
     dishes = fp.create_dishes_two()
     assert len(dishes) == 4
     assert dishes[0] == ('rigatoni', 'al pesto')
@@ -60,6 +81,9 @@ def test_two_for_loops_listcomp_two():
 
 
 def test_my_first_genexps_example():
+    """
+    genexp, unpacking, formatting
+    """
     dishes = fp.create_dishes_three()
     # assert len(dishes) == 4
     # TypeError: object of type 'generator' has no len()
@@ -71,28 +95,31 @@ def test_my_first_genexps_example():
     ]
     i = 0
     print()
-    for d in dishes:
-        assert d == expected_dishes[i]
+    for dish in dishes:
+        assert dish == expected_dishes[i]
         i += 1
 
 
 def test_tuples_one():
+    """
+    The % formatting operator understands tuples and
+    treats each item as a separate field. See for loop below.
+    """
     calling_codes = [('Afghanistan', '+93'), ('Australia', '+61'), ]
     i = 0
     for calling_code in calling_codes:
-        """
-        The % formatting operator understands tuples and
-        treats each item as a separate field.
-        """
-        cc = '%s: %s' % calling_code
+        concated_cc = '%s: %s' % calling_code
         if i == 0:
-            assert cc == 'Afghanistan: +93'
+            assert concated_cc == 'Afghanistan: +93'
         else:
-            assert cc == 'Australia: +61'
+            assert concated_cc == 'Australia: +61'
         i += 1
 
 
 def test_tuples_two():
+    """
+    How to only use one part of the tuple.
+    """
     calling_codes = [('Afghanistan', '+93'), ('Australia', '+61'), ]
     i = 0
     for country, _ in calling_codes:
@@ -104,6 +131,9 @@ def test_tuples_two():
 
 
 def test_tuples_swap():
+    """
+    Trick to swap variables
+    """
     first = 'Samuel'
     second = 'Bächler'
     first, second = second, first
@@ -112,8 +142,11 @@ def test_tuples_swap():
 
 
 def test_prefixing_with_star():
-    t = (20, 8)
-    quotient, remainder = divmod(*t)
+    """
+    Usage of star (asterisk) as method argument.
+    """
+    t_1 = (20, 8)
+    quotient, remainder = divmod(*t_1)
     assert quotient == 2
     assert remainder == 4
 
@@ -133,31 +166,40 @@ def test_if_object_is_changeable():
     assert b_true == fp.fixed(unchangeable_tuple)
 
 
-s = '0123456789'
+S = '0123456789'
 
 
 def test_slicing_example():
+    """
+    Slicing
+    """
     step = 2
-    assert s[::step] == '02468'
+    assert S[::step] == '02468'
     start = 7
-    assert s[start::step] == '79'
+    assert S[start::step] == '79'
     step = 3
-    assert s[::step] == '0369'
+    assert S[::step] == '0369'
     stop = 7
-    assert s[:stop:step] == '036'
-    assert s[::10] == '0'
+    assert S[:stop:step] == '036'
+    assert S[::10] == '0'
 
 
 def test_slicing_in_reverse():
-    assert s[::-1] == '9876543210'
-    assert s[3::-1] == '3210'
-    assert s[::-2] == '97531'
-    assert s[3::-2] == '31'
-    assert s[::-3] == '9630'
-    assert s[::10] == '0'
+    """
+    Slicing in reverse
+    """
+    assert S[::-1] == '9876543210'
+    assert S[3::-1] == '3210'
+    assert S[::-2] == '97531'
+    assert S[3::-2] == '31'
+    assert S[::-3] == '9630'
+    assert S[::10] == '0'
 
 
 def test_slice_method_example():
+    """
+    How slice can be used to handle fixed with formatted lines.
+    """
     row = '01234567890123456789'
     row = 'Samuel Bächler    42'
     first_name = slice(0, 7)
@@ -168,13 +210,15 @@ def test_slice_method_example():
     assert row[age] == '42'
 
 
-"""
+def test_mute_pylint_warning():
+    """
+    In case comment below is not in this bogus function pylint will complain
+    'String statement has no effect [pointless-string-statement]'.
 
-In fp.ipynb in the notebooks section there are examples on
+    In fp.ipynb in the notebooks section there are examples on
 
-- Grabbing Excess Items: a, b, *rest = range(5)
-- Grabbing Excess Items: a, *middle, b = range(5)
-- Nested Unpacking: person, age, height, (father, mother) in persons
-- Splitting: my_list[:2]
-
-"""
+        - Grabbing Excess Items: a, b, *rest = range(5)
+        - Grabbing Excess Items: a, *middle, b = range(5)
+        - Nested Unpacking: person, age, height, (father, mother) in persons
+        - Splitting: my_list[:2]
+    """
