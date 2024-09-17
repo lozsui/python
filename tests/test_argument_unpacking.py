f04cb877a1aeb3b2b521816e87f34805943780ed
Samuel Bächler <samuel.baechler@ewz.ch>
2024-09-17 17:15:36 +0200

    get the grip of kwargs

--- tests/test_argument_unpacking.py
+++ tests/test_argument_unpacking.py
@@ -30,4 +30,18 @@ def test_unpacking():
 def test_add_up_numbers():
     assert add_up_numbers() == 0
     assert add_up_numbers(1,2) == 3
-    assert add_up_numbers(1,2,3) == 6    
+    assert add_up_numbers(1,2,3) == 6
+
+def concat_kw_args(**kwargs):
+    concated_str = ""
+    for k, v in kwargs.items():
+        concated_str += f"{str(k)}: {v} "
+    return concated_str
+
+def test_kw_args():
+    result = concat_kw_args(person_1="Sam", person_2="Hans")
+    assert "person_1: Sam person_2: Hans " == result
+
+def test_kw_args_two():
+    result = concat_kw_args(person_1="Sam", person_2="Hans", person_3="Hermann")
+    assert "person_1: Sam person_2: Hans person_3: Hermann " == result
