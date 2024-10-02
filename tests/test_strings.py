8673d3cd3e2e7658e10b1ff6c91114b2cc23cd61
Samuel Bächler <samuel.baechler@ewz.ch>
2024-10-02 17:09:56 +0200

    example of a json like string

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -49,6 +49,13 @@ def test_format_string2():
     greeting = "Hello {name}!"
     assert greeting.format(name=name) == "Hello lozsui!"
 
+def test_json_string():
+    hostname = "HOSTNAME"
+    port = 4242
+    config = """{{"credentials":{{"username":"test_user","password":"test_pwd"}},"hostname":"{hostname}","port":"{port}"}}""".format(hostname=hostname, port=port)
+    print(config)
+    assert config == """{"credentials":{"username":"test_user","password":"test_pwd"},"hostname":"HOSTNAME","port":"4242"}"""
+
 def test_multiply_string():
     my_str = "3"
     assert my_str * 3 == "333"
